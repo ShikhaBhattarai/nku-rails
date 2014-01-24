94e536e2f4a0388d781b2722385bce0f87394a16
bhattarai <bhattarais1@nku.edu>
2014-01-24 00:44:20 +0000

    Created a list for the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
-def new
+  def new
+  end
+  
+  def show
+  @post = Post.find(params[:id])
+  end
+  
+  def index
+  @posts = Post.all
+  end
+ 
+  def create
+  @post = Post.new(post_params)
+ 
+  @post.save
+  redirect_to @post
+  end
+  
+  def index
+  @posts = Post.all
+  end
+ 
+  private
+  def post_params
+  params.require(:post).permit(:title, :text)
 end
 end
