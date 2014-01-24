bc91847596fa43253bfb3c91b8432dc3f6eaad61
bhattarai <bhattarais1@nku.edu>
2014-01-24 01:02:23 +0000

    Adding validation to the form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,19 @@ class PostsController < ApplicationController
   def index
   @posts = Post.all
   end
+  def new
+  @post = Post.new
+end
+ 
+  def create
+  @post = Post.new(params[:post].permit(:title, :text))
+ 
+  if @post.save
+    redirect_to @post
+  else
+    render 'new'
+  end
+  end
  
   private
   def post_params
